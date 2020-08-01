fe3bf9f90f4c678e812587fbb8ba32b4c0dabb3d
Massoud-Ac <Massoud.ac71@gmail.com>
2020-08-01 16:30:33 +0430

    first-commit

--- src/FakeData/GridData.js
+++ src/FakeData/GridData.js
@@ -3,306 +3,164 @@ export default [
     date: ' 1399',
     startTime: '15:20',
     endTime: '18:30',
-    project: 'دیسپچینگ',
+    project: 'شرکت',
     WBS: 'کار بر روی Ui',
     activity: "توسعه",
-    description: "یا تاااااااااااااااقان"
+    description: "تست "
   },
   {
     date: ' 1399',
     startTime: '15:20',
     endTime: '18:30',
-    project: 'دیسپچینگ',
+    project: 'شرکت',
     WBS: 'کار بر روی Ui',
     activity: "توسعه",
-    description: "یا تاااااااااااااااقان"
+    description: "تست "
   },
   {
     date: ' 1399',
     startTime: '15:20',
     endTime: '18:30',
-    project: 'دیسپچینگ',
+    project: 'شرکت',
     WBS: 'کار بر روی Ui',
     activity: "توسعه",
-    description: "یا تاااااااااااااااقان"
+    description: "تست "
   },
   {
     date: ' 1399',
     startTime: '15:20',
     endTime: '18:30',
-    project: 'دیسپچینگ',
+    project: 'شرکت',
     WBS: 'کار بر روی Ui',
     activity: "توسعه",
-    description: "یا تاااااااااااااااقان"
+    description: "تست "
   },
   {
     date: ' 1399',
     startTime: '15:20',
     endTime: '18:30',
-    project: 'دیسپچینگ',
+    project: 'شرکت',
     WBS: 'کار بر روی Ui',
     activity: "توسعه",
-    description: "یا تاااااااااااااااقان"
+    description: "تست "
   },
   {
     date: ' 1399',
     startTime: '15:20',
     endTime: '18:30',
-    project: 'دیسپچینگ',
+    project: 'شرکت',
     WBS: 'کار بر روی Ui',
     activity: "توسعه",
-    description: "یا تاااااااااااااااقان"
+    description: "تست "
   },
   {
     date: ' 1399',
     startTime: '15:20',
     endTime: '18:30',
-    project: 'دیسپچینگ',
+    project: 'شرکت',
     WBS: 'کار بر روی Ui',
     activity: "توسعه",
-    description: "یا تاااااااااااااااقان"
+    description: "تست "
   },
   {
     date: ' 1399',
     startTime: '15:20',
     endTime: '18:30',
-    project: 'دیسپچینگ',
+    project: 'شرکت',
     WBS: 'کار بر روی Ui',
     activity: "توسعه",
-    description: "یا تاااااااااااااااقان"
+    description: "تست "
   },
   {
     date: ' 1399',
     startTime: '15:20',
     endTime: '18:30',
-    project: 'دیسپچینگ',
+    project: 'شرکت',
     WBS: 'کار بر روی Ui',
     activity: "توسعه",
-    description: "یا تاااااااااااااااقان"
+    description: "تست "
   },
   {
     date: ' 1399',
     startTime: '15:20',
     endTime: '18:30',
-    project: 'دیسپچینگ',
+    project: 'شرکت',
     WBS: 'کار بر روی Ui',
     activity: "توسعه",
-    description: "یا تاااااااااااااااقان"
+    description: "تست "
   },
   {
     date: ' 1399',
     startTime: '15:20',
     endTime: '18:30',
-    project: 'دیسپچینگ',
+    project: 'شرکت',
     WBS: 'کار بر روی Ui',
     activity: "توسعه",
-    description: "یا تاااااااااااااااقان"
+    description: "تست "
   },
   {
     date: ' 1399',
     startTime: '15:20',
     endTime: '18:30',
-    project: 'دیسپچینگ',
+    project: 'شرکت',
     WBS: 'کار بر روی Ui',
     activity: "توسعه",
-    description: "یا تاااااااااااااااقان"
-  },
-  {
+    description: "تست "
+  }, {
     date: ' 1399',
     startTime: '15:20',
     endTime: '18:30',
-    project: 'دیسپچینگ',
+    project: 'شرکت',
     WBS: 'کار بر روی Ui',
     activity: "توسعه",
-    description: "یا تاااااااااااااااقان"
-  },
-  {
+    description: "تست "
+  }, {
     date: ' 1399',
     startTime: '15:20',
     endTime: '18:30',
-    project: 'دیسپچینگ',
+    project: 'شرکت',
     WBS: 'کار بر روی Ui',
     activity: "توسعه",
-    description: "یا تاااااااااااااااقان"
-  },
-  {
+    description: "تست "
+  }, {
     date: ' 1399',
     startTime: '15:20',
     endTime: '18:30',
-    project: 'دیسپچینگ',
+    project: 'شرکت',
     WBS: 'کار بر روی Ui',
     activity: "توسعه",
-    description: "یا تاااااااااااااااقان"
-  },
-  {
+    description: "تست "
+  }, {
     date: ' 1399',
     startTime: '15:20',
     endTime: '18:30',
-    project: 'دیسپچینگ',
+    project: 'شرکت',
     WBS: 'کار بر روی Ui',
     activity: "توسعه",
-    description: "یا تاااااااااااااااقان"
-  },
-  {
+    description: "تست "
+  }, {
     date: ' 1399',
     startTime: '15:20',
     endTime: '18:30',
-    project: 'دیسپچینگ',
+    project: 'شرکت',
     WBS: 'کار بر روی Ui',
     activity: "توسعه",
-    description: "یا تاااااااااااااااقان"
-  },
-  {
+    description: "تست "
+  }, {
     date: ' 1399',
     startTime: '15:20',
     endTime: '18:30',
-    project: 'دیسپچینگ',
+    project: 'شرکت',
     WBS: 'کار بر روی Ui',
     activity: "توسعه",
-    description: "یا تاااااااااااااااقان"
-  },
-  {
+    description: "تست "
+  }, {
     date: ' 1399',
     startTime: '15:20',
     endTime: '18:30',
-    project: 'دیسپچینگ',
+    project: 'شرکت',
     WBS: 'کار بر روی Ui',
     activity: "توسعه",
-    description: "یا تاااااااااااااااقان"
-  },
-  {
-    date: ' 1399',
-    startTime: '15:20',
-    endTime: '18:30',
-    project: 'دیسپچینگ',
-    WBS: 'کار بر روی Ui',
-    activity: "توسعه",
-    description: "یا تاااااااااااااااقان"
-  },
-  {
-    date: ' 1399',
-    startTime: '15:20',
-    endTime: '18:30',
-    project: 'دیسپچینگ',
-    WBS: 'کار بر روی Ui',
-    activity: "توسعه",
-    description: "یا تاااااااااااااااقان"
-  },
-  {
-    date: ' 1399',
-    startTime: '15:20',
-    endTime: '18:30',
-    project: 'دیسپچینگ',
-    WBS: 'کار بر روی Ui',
-    activity: "توسعه",
-    description: "یا تاااااااااااااااقان"
-  },
-  {
-    date: ' 1399',
-    startTime: '15:20',
-    endTime: '18:30',
-    project: 'دیسپچینگ',
-    WBS: 'کار بر روی Ui',
-    activity: "توسعه",
-    description: "یا تاااااااااااااااقان"
-  },
-  {
-    date: ' 1399',
-    startTime: '15:20',
-    endTime: '18:30',
-    project: 'دیسپچینگ',
-    WBS: 'کار بر روی Ui',
-    activity: "توسعه",
-    description: "یا تاااااااااااااااقان"
-  },
-  {
-    date: ' 1399',
-    startTime: '15:20',
-    endTime: '18:30',
-    project: 'دیسپچینگ',
-    WBS: 'کار بر روی Ui',
-    activity: "توسعه",
-    description: "یا تاااااااااااااااقان"
-  },
-  {
-    date: ' 1399',
-    startTime: '15:20',
-    endTime: '18:30',
-    project: 'دیسپچینگ',
-    WBS: 'کار بر روی Ui',
-    activity: "توسعه",
-    description: "یا تاااااااااااااااقان"
-  },
-  {
-    date: ' 1399',
-    startTime: '15:20',
-    endTime: '18:30',
-    project: 'دیسپچینگ',
-    WBS: 'کار بر روی Ui',
-    activity: "توسعه",
-    description: "یا تاااااااااااااااقان"
-  },
-  {
-    date: ' 1399',
-    startTime: '15:20',
-    endTime: '18:30',
-    project: 'دیسپچینگ',
-    WBS: 'کار بر روی Ui',
-    activity: "توسعه",
-    description: "یا تاااااااااااااااقان"
-  },
-  {
-    date: ' 1399',
-    startTime: '15:20',
-    endTime: '18:30',
-    project: 'دیسپچینگ',
-    WBS: 'کار بر روی Ui',
-    activity: "توسعه",
-    description: "یا تاااااااااااااااقان"
-  },
-  {
-    date: ' 1399',
-    startTime: '15:20',
-    endTime: '18:30',
-    project: 'دیسپچینگ',
-    WBS: 'کار بر روی Ui',
-    activity: "توسعه",
-    description: "یا تاااااااااااااااقان"
-  },
-  {
-    date: ' 1399',
-    startTime: '15:20',
-    endTime: '18:30',
-    project: 'دیسپچینگ',
-    WBS: 'کار بر روی Ui',
-    activity: "توسعه",
-    description: "یا تاااااااااااااااقان"
-  },
-  {
-    date: ' 1399',
-    startTime: '15:20',
-    endTime: '18:30',
-    project: 'دیسپچینگ',
-    WBS: 'کار بر روی Ui',
-    activity: "توسعه",
-    description: "یا تاااااااااااااااقان"
-  },
-  {
-    date: ' 1399',
-    startTime: '15:20',
-    endTime: '18:30',
-    project: 'دیسپچینگ',
-    WBS: 'کار بر روی Ui',
-    activity: "توسعه",
-    description: "یا تاااااااااااااااقان"
-  },
-  {
-    date: ' 1399',
-    startTime: '15:20',
-    endTime: '18:30',
-    project: 'دیسپچینگ',
-    WBS: 'کار بر روی Ui',
-    activity: "توسعه",
-    description: "یا تاااااااااااااااقان"
+    description: "تست "
   },
 ]
